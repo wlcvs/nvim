6e6f3c64c9c72a40a9a1d07f38dc5c99aaa2ecc5
Wallacy V. da Silva <wlcsv8080@outlook.com>
2026-03-16 19:49:40 -0300

    First commit.

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,32 @@
+return {
+    'nvim-treesitter/nvim-treesitter',
+    lazy = false,
+    build = ':TSUpdate',
+
+    config = function()
+	local ts = require("nvim-treesitter")
+
+	ts.setup({
+	    install_dir = vim.fn.stdpath("data") .. "/site",
+	    highlight = { enable = true },
+	    indent = { enable = true },
+	})
+
+	ts.install({
+	    "lua",
+	    "php",
+	    "javascript",
+	    "java",
+	    "typescript",
+	    "python",
+	    "rust",
+	    "c",
+	    "cpp",
+	    "sql",
+	    "css",
+	    "html",
+	    "tsx",
+	    "haskell",
+	})
+    end
+}
