66d768ab5ddc8d1650debefdad325c87ec44278f
Wallacy <wlcsv8080@outlook.com>
2026-03-23 20:31:36 -0300

    Fixing the indentation.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,33 +1,33 @@
 return {
-    'nvim-treesitter/nvim-treesitter',
-    lazy = false,
-    build = ':TSUpdate',
+  'nvim-treesitter/nvim-treesitter',
+  lazy = false,
+  build = ':TSUpdate',
 
-    config = function()
-	local ts = require("nvim-treesitter")
+  config = function()
+    local ts = require("nvim-treesitter")
 
-	ts.setup({
-	    install_dir = vim.fn.stdpath("data") .. "/site",
-	    highlight = { enable = true },
-	    indent = { enable = true },
-	})
+    ts.setup({
+      install_dir = vim.fn.stdpath("data") .. "/site",
+      highlight = { enable = true },
+      indent = { enable = true },
+    })
 
-	ts.install({
-	    "lua",
-	    "php",
-	    "javascript",
-	    "markdown",
-	    "java",
-	    "typescript",
-	    "python",
-	    "rust",
-	    "c",
-	    "cpp",
-	    "sql",
-	    "css",
-	    "html",
-	    "tsx",
-	    "haskell",
-	})
-    end
+    ts.install({
+      "lua",
+      "php",
+      "javascript",
+      "markdown",
+      "java",
+      "typescript",
+      "python",
+      "rust",
+      "c",
+      "cpp",
+      "sql",
+      "css",
+      "html",
+      "tsx",
+      "haskell",
+    })
+  end
 }
